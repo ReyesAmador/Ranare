35f343a29fde4b29410c6f498dcddb40cf5faf63
Reyes <ramador@alumnat.copernic.cat>
2024-11-19 09:28:52 +0100

    primer commit para que hagan pull los demás miembros

---
+++ src/main/java/cat/copernic/ranare/RanareApplication.java
@@ -0,0 +1,13 @@
+package cat.copernic.ranare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/copernic/ranare/RanareApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.copernic.ranare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
