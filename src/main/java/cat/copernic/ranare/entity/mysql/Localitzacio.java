bf822d6919ee5547d3cfca97e3cb08966189858b
Reyes <ramador@alumnat.copernic.cat>
2024-11-21 13:58:08 +0100

    hecho JavaDoc de Localització y añadido atributo horari tancament

--- src/main/java/cat/copernic/ranare/entity/mysql/Localitzacio.java
+++ src/main/java/cat/copernic/ranare/entity/mysql/Localitzacio.java
@@ -20,7 +20,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- *
+ * Representa una localització dins del sistema, amb la seva informació com el codi postal, 
+ * adreça, tipus de localització, horari d'obertura i de tancament.
+ * Una localització està relacionada amb un agent (relació un a un) i amb diversos vehicles 
+ * (relació un a molts).
+ * 
+ * La classe està mapejada a una taula de la base de dades utilitzant JPA.
+ * Utilitza les anotacions de Lombok per generar automàticament els mètodes getters, setters, constructors, 
+ * i altres mètodes útils per la classe.
+ * 
  * @author reyes
  */
 @Entity
@@ -35,14 +43,30 @@ public class Localitzacio {
     
     private String adreca;
     
+    /**
+     * Tipus de localització (pot ser una parking, un carrer, un aeroport, etc.).
+     */
     private String tipus;
     
-    private LocalDateTime horari;
+    @Column(name = "horari_apertura")
+    private LocalDateTime horariApertura;
     
+    @Column(name = "horari_tancament")
+    private LocalDateTime horariTancament;
+    
+    
+    /**
+     * Agent assignat a aquesta localització. Relació un a un amb la classe {@link Agent}.
+     * El camp `dni_agent` es refereix a la clau primària de l'entitat {@link Agent} (el camp `dni`).
+     */
     @OneToOne
     @JoinColumn(name = "dni_agent", referencedColumnName = "dni")
     private Agent agent;
     
+    /**
+     * Vehicles associats a aquesta localització. Relació un a molts amb la classe {@link Vehicle}.
+     * Les operacions de persistència en una localització també es propagaran als vehicles associats.
+     */
     @OneToMany(mappedBy = "localitzacio", cascade = CascadeType.ALL)
     private Set<Vehicle> vehicles;
 }
