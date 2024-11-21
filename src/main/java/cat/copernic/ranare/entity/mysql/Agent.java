feb4e476bb8b9054f1a0c1f8acfd61178f5559a0
Reyes <ramador@alumnat.copernic.cat>
2024-11-21 13:52:27 +0100

    añadida clase enum ROL, añadidas relaciones en localitzacio y Agent

--- src/main/java/cat/copernic/ranare/entity/mysql/Agent.java
+++ src/main/java/cat/copernic/ranare/entity/mysql/Agent.java
@@ -4,27 +4,41 @@
  */
 package cat.copernic.ranare.entity.mysql;
 
+import cat.copernic.ranare.enums.Rol;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- *
+ * Representa un agent dins del sistema, amb la seva informació personal i el rol assignat.
+ * Un agent està relacionat amb una localització, i pot ser assignat a una sola localització a través de la relació un a un.
+ * 
+ * La classe està mapejada a una taula de la base de dades utilitzant JPA.
+ * Utilitza les anotacions de Lombok per generar automàticament els mètodes getters, setters, constructors, 
+ * i altres mètodes útils per la classe.
+ * 
+ * 
+ * La relació amb la classe {@link Localitzacio} és de tipus un a un, i l'entitat {@link Localitzacio} 
+ * té el camp `agent` que fa referència a aquesta classe.
+ * 
  * @author reyes
  */
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Agent {
     
+    /**
+     * DNI de l'agent. Aquest camp és la clau primària de l'entitat {@link Agent}.
+     */
     @Id
     private String dni;
     
@@ -34,7 +48,20 @@ public class Agent {
     
     private String email;
     
-    @OneToOne(cascade = CascadeType.REMOVE)
-    @JoinColumn(name = "localitzacio")
+    /**
+     * Rol de l'agent, determinat per un valor de l'enumeració {@link Rol}.
+     */
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+    
+    
+    /**
+     * Localització assignada a l'agent. Relació un a un amb la classe {@link Localitzacio}.
+     * La propietat `mappedBy` indica que aquesta relació és gestionada per la classe {@link Localitzacio}.
+     * 
+     * {@link CascadeType.ALL} indica que les operacions de persistència en un agent també 
+     * es propagaran a la localització associada.
+     */
+    @OneToOne(mappedBy = "agent",cascade = CascadeType.ALL)
     private Localitzacio localitzacio;
 }
--- src/main/java/cat/copernic/ranare/entity/mysql/Localitzacio.java
+++ src/main/java/cat/copernic/ranare/entity/mysql/Localitzacio.java
@@ -4,11 +4,17 @@
  */
 package cat.copernic.ranare.entity.mysql;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDateTime;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,4 +34,15 @@ public class Localitzacio {
     private Integer codiPostal;
     
     private String adreca;
+    
+    private String tipus;
+    
+    private LocalDateTime horari;
+    
+    @OneToOne
+    @JoinColumn(name = "dni_agent", referencedColumnName = "dni")
+    private Agent agent;
+    
+    @OneToMany(mappedBy = "localitzacio", cascade = CascadeType.ALL)
+    private Set<Vehicle> vehicles;
 }
