9f46ee3a9a7f64a681023e9024aa020e4f179228
Reyes Amador Jurado <ramador@alumnat.copernic.cat>
2024-11-22 13:15:38 +0000

    Revert "Merge branch 'feature/creacio_index' into 'master'"
    This reverts merge request !1

--- src/main/java/cat/copernic/repository/NewClass.java
+++ src/main/java/cat/copernic/repository/NewClass.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package cat.copernic.ranare.enums;
+package cat.copernic.repository;
 
 /**
  *
- * @author ngall
+ * @author reyes
  */
-public enum TipusCombustio {
-    ELECTRIC, HIBRID, GASOIL
+public class NewClass {
+    
 }
