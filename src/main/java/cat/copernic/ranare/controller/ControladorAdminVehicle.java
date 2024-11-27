2f38cccf39a391567a270fed54a7729c7affa8c5
nacho <ngallardo@alumnat.copernic.cat>
2024-11-27 09:04:03 +0100

    He creat el html, el controlador i servei necessari per la consulta dels vehicles a la pàgina de admin.

---
+++ src/main/java/cat/copernic/ranare/controller/ControladorAdminVehicle.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.copernic.ranare.controller;
+
+import cat.copernic.ranare.entity.mysql.Vehicle;
+import cat.copernic.ranare.service.mysql.AdminVehicleService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ *
+ * @author ngall
+ */
+
+@Controller
+public class ControladorAdminVehicle {
+    
+    @Autowired
+    private AdminVehicleService adminVehicleService;
+    
+    @GetMapping("/admin/vehicles")
+    public String mostrarVehicles(Model model){
+        List<Vehicle> vehicles = adminVehicleService.getAllVehicles();
+        model.addAttribute("vehicles", vehicles);
+        return "admin-vehicles";
+    }
+    
+    @GetMapping("/admin/seleccionarVehicle")
+    public String seleccionarVehicle(@RequestParam(name = "matricula", required = false) 
+            String matricula, RedirectAttributes redirectAttributes){
+        if(matricula != null && !matricula.isEmpty()){
+            return "redirect:/crear-vehicle?matricula=" + matricula;
+        }else{
+            return "redirect:/crear-vehicle";
+        }
+    }
+    
+    @PostMapping("/admin/eliminarVehicle")
+    public String eliminarVehicle(@RequestParam(name = "matricula", required = true)
+            String matricula, RedirectAttributes redirectAttributes){
+        if(matricula != null && !matricula.isEmpty()){
+            adminVehicleService.deleteVehicle(matricula);
+            redirectAttributes.addFlashAttribute("message", "Vehicle eliminat correctament.");
+        }else{
+            redirectAttributes.addFlashAttribute("errorMessage","No s'ha seleccionat cap vehicle per eliminar.");
+        }
+        return "redirect:/admin/vehicles";
+    }
+    
+}
